ed81ed638d738f6b25bb62697ea6543177d00511
Karllos26 <karlloseduardopereira@gmail.com>
2023-11-09 20:38:45 -0300

    Page pesquisa

---
+++ src/pages/pesquisa/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "../../components/Layout";
+import SearchPage from "../../components/Pesquisa";
+
+export default function LayoutPesquisa(){
+    return(
+        <Layout>
+            <SearchPage/>
+        </Layout>
+    )
+}
