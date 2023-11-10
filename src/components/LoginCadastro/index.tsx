f49ad5fa9dfece2f5c3b75a949cb0da13c283728
Karllos Eduardo Rodrigues Pereira <karlloseduardopereira@gmail.com>
2023-11-09 23:31:20 -0300

    deleted

--- src/components/LoginCadastro/index.tsx
+++
@@ -1,5 +0,0 @@
-// authUtils.ts
-export const isUserAuthenticated = () => {
-  const token = localStorage.getItem('token');
-  return !!token; // Retorna true se houver um token, caso contrário, retorna false
-};
