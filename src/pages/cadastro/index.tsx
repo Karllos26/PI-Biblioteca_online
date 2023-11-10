4730168965e07d38994de2f0839bd109248746ab
Gustavo Vendrami do Amaral <143762029+gvendrami@users.noreply.github.com>
2023-11-09 23:08:22 -0300

    Add files via upload

---
+++ src/pages/cadastro/index.tsx
@@ -0,0 +1,10 @@
+import Cadastro from "../../components/HomeComponents/LoginCadastro/Cadastro";
+import Layout from "../../components/Layout";
+
+export default function CadastroPage(){
+    return(
+    <Layout>
+        <Cadastro/>
+    </Layout>
+    )
+}
