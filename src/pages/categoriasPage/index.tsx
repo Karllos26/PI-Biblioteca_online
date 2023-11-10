f4ae920a8648a0e965c720c13b07983f4fbb94d2
Luccas Yuri dos Santos <66194181+LuccasYuridosSantos@users.noreply.github.com>
2023-11-09 22:53:27 -0300

    Add files via upload
    Add categoriasPage folder and files

---
+++ src/pages/categoriasPage/index.tsx
@@ -0,0 +1,11 @@
+import CategoriaPage from "../../components/HomeComponents/CategoriaHome";
+import Layout from "../../components/Layout";
+
+export default function CategoriasHome() {
+  
+    return (
+  <Layout>
+ <CategoriaPage/>
+  </Layout>
+    )
+  }
