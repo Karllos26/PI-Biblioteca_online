fc48c1ef8b5881c08289575fb921305874254f81
Karllos Eduardo Rodrigues Pereira <karlloseduardopereira@gmail.com>
2023-11-16 17:09:55 -0300

    pequena mudanca

--- src/components/HomeComponents/NewHome/index.tsx
+++ src/components/HomeComponents/NewHome/index.tsx
@@ -3,12 +3,6 @@ import CardsHome from "../CardsHome";
 import HomeListaCategoria from "../ListCatgoryHome";
 import ScrollToTopButton from "../VoltarTopo";
 
-const pageStyle = {
-  width: '100vw',  // Largura igual à largura da tela
-  height: '100vh',  // Altura igual à altura da tela
-  overflow: 'hidden',  // Esconde a barra de rolagem
-};
-
 export default function HomeBiblioteca() {
   return(
   <Box maxWidth="100%" overflowX="hidden">
