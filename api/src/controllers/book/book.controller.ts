168baa511d3971dc0756099374c9548dda3c8b92
Isac Narcizo Baroni Bazzani <106942461+IsacI@users.noreply.github.com>
2023-11-23 22:25:04 -0300

    Add files via upload

--- api/src/controllers/book/book.controller.ts
+++ api/src/controllers/book/book.controller.ts
@@ -21,5 +21,90 @@ export default class BookController {
     return res.status(201).json(book)
   }
 
+  static async getAllBooks(req: Request, res: Response) {
+    try {
+      const books = await Book.find();
+      return res.json(books);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+  }
+
+  static async getBookById(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const book = await Book.findOneBy({id: Number(id)});
+
+      if (!book) {
+        return res.status(404).json({ error: 'Livro não encontrado' });
+      }
+
+      return res.json(book);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+  }
+
+  static async getBooksByCategory(req: Request, res: Response) {
+    try {
+      const { category } = req.params;
+      const books = await Book.findBy({ category: category });
   
+      return res.json(books);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+  }
+
+  static async getBooksByName(req: Request, res: Response) {
+    try {
+      const { name } = req.params;
+      const books = await  Book.findBy({ heading: name });
+  
+      return res.json(books);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+  }
+
+  static async deleteBook(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const deletedBook = await Book.delete(id);
+
+      if (!deletedBook) {
+        return res.status(404).json({ error: 'Livro não encontrado' });
+      }
+
+      return res.json(deletedBook);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+  }
+
+  static async updateBook(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const { heading, imageUrl, description, href, category, rating } = req.body;
+
+      const updatedBook = await Book.update(
+        { id: Number(id) },
+        { heading, imageUrl, description, href, category, rating }
+      );
+
+      if (!updatedBook) {
+        return res.status(404).json({ error: 'Livro não encontrado' });
+      }
+
+      return res.json(updatedBook);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+  }
 }
