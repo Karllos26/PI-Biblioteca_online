67acfec8b6bc7006e492f5fdaf1f6e68083d9a29
Isac Narcizo Baroni Bazzani <106942461+IsacI@users.noreply.github.com>
2023-11-23 22:29:59 -0300

    Add files via upload

--- api/src/routes/book/book.routes.ts
+++ api/src/routes/book/book.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 import BookController from '../../controllers/book/book.controller'
 
-const taskRoutes = Router()
+const router = Router()
 
-taskRoutes.post('/', BookController.register)
-taskRoutes.get('/', BookController.list)
+router.post('/books', BookController.register);
+router.get('/books', BookController.getAllBooks);
+router.get('/books/:id', BookController.getBookById);
+router.get('/books/category/:category', BookController.getBooksByCategory);
+router.get('/books', BookController.getBooksByName);
+router.delete('/books/:id', BookController.deleteBook);
+router.put('/books/:id', BookController.updateBook);
 
 
-export default taskRoutes
+export default router
