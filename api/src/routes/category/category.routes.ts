d6101bca8ff342966c9ab3454fc110950c904033
BDdeSouza <105115221+BDdeSouza@users.noreply.github.com>
2023-11-23 22:37:03 -0300

    Add files via upload

--- api/src/routes/category/category.routes.ts
+++ api/src/routes/category/category.routes.ts
@@ -1,8 +1,21 @@
 import { Router } from 'express'
 import CategoryController from '../../controllers/category/category.controller'
 
-const taskRoutes = Router()
+const router = Router()
 
-taskRoutes.post('/', CategoryController.store)
+// Criar uma categoria
+router.post('/categories', CategoryController.createCategory);
 
-export default taskRoutes
+// Obter todas as categorias
+router.get('/categories', CategoryController.getAllCategories);
+
+// Obter uma categoria por ID
+router.get('/categories/:id', CategoryController.getCategoryById);
+
+// Atualizar uma categoria por ID
+router.put('/categories/:id', CategoryController.updateCategory);
+
+// Excluir uma categoria por ID
+router.delete('/categories/:id', CategoryController.deleteCategory);
+
+export default router;
